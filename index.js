2c85f7dbab5738a191d8561d6593824c4c9c3dae
syedquadri82 <syed.quadri82@gmail.com>
2021-03-19 10:30:04 +1100

    reorganized code

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
 const axios = require('axios');
 var defaultUrlAddress = '/api/products/1'
 const conversionFactor = 250
+const apiUrl = 'http://wp8m3he1wt.s3-website-ap-southeast-2.amazonaws.com'
+const filterItem = 'Air Conditioners'
 var itemCount = 0
 var averageCubicWeight = 0
 
 function getProducts(urlAddress)
 {
-    axios.get('http://wp8m3he1wt.s3-website-ap-southeast-2.amazonaws.com' + urlAddress).then(resp => {
+    axios.get(apiUrl + urlAddress).then(resp => {
 
         Object.entries(resp.data.objects).forEach(([key, value]) => 
         {
-            if(value.category === 'Air Conditioners')
+            if(value.category === filterItem)
             {   itemCount++
                 averageCubicWeight += ((value.size.width / 100) * (value.size.length / 100) * (value.size.height / 100) * (conversionFactor))                
-                console.log('\n*** Air Conditioner: ' + value.title + ' ***')
+                console.log('\n*** ' + value.title + ' ***')
                 console.log('Width: ' + value.size.width / 100)
                 console.log('Length: ' + value.size.length / 100)
                 console.log('Height: ' + value.size.height / 100)
-                console.log('Cubic Weight: ' + ((value.size.width / 100) * (value.size.length / 100) * (value.size.height / 100) * (conversionFactor)))
+                console.log('Cubic Weight: ' + ((value.size.width / 100) * (value.size.length / 100) * (value.size.height / 100) * (conversionFactor)) + ' Kg')
                 
             }
         }
@@ -30,7 +32,7 @@ function getProducts(urlAddress)
         }
         else
         {
-            console.log('\nComputed Average Cubic Weight: ' + Math.round((averageCubicWeight / itemCount + Number.EPSILON) * 100) / 100 + '\n')
+            console.log('\nComputed Average Cubic Weight for ' +filterItem+ ': ' + Math.round((averageCubicWeight / itemCount + Number.EPSILON) * 100) / 100 + ' Kg\n')
 
         }
     });
