a4ca0bd52c8715bd711f4fd1b5a73e8d16a97291
syedquadri82 <syed.quadri82@gmail.com>
2021-03-20 11:05:08 +1100

    added comments and restructured code

--- index.js
+++ index.js
@@ -1,42 +1,55 @@
 const axios = require('axios');
-var defaultUrlAddress = '/api/products/1'
+var defaultUrlPart = '/api/products/1'
 const conversionFactor = 250
-const apiUrl = 'http://wp8m3he1wt.s3-website-ap-southeast-2.amazonaws.com'
+const baseUrl = 'http://wp8m3he1wt.s3-website-ap-southeast-2.amazonaws.com'
 const filterItem = 'Air Conditioners'
-var itemCount = 0
-var averageCubicWeight = 0
+var itemCount = 0 //cumulative
+var itemCubicWeight = 0 //cumulative
+var hasError = false;
 
-function getProducts(urlAddress)
+function computePrdAvgCuWgt(urlPart, productName)
 {
-    axios.get(apiUrl + urlAddress).then(resp => {
-
+    //fetch products from api
+    axios.get(baseUrl + urlPart).then(resp => {
+        //Loop through each product returned in response
         Object.entries(resp.data.objects).forEach(([key, value]) => 
         {
-            if(value.category === filterItem)
-            {   itemCount++
-                averageCubicWeight += ((value.size.width / 100) * (value.size.length / 100) * (value.size.height / 100) * (conversionFactor))                
-                console.log('\n*** ' + value.title + ' ***')
-                console.log('Width: ' + value.size.width / 100)
-                console.log('Length: ' + value.size.length / 100)
-                console.log('Height: ' + value.size.height / 100)
-                console.log('Cubic Weight: ' + ((value.size.width / 100) * (value.size.length / 100) * (value.size.height / 100) * (conversionFactor)) + ' Kg')
-                
+            if(value.category === productName)
+            {    
+                try
+                {
+                    itemCount++ //keep count of items to calculate average at end of call
+                    itemCubicWeight += ((value.size.width / 100) * (value.size.length / 100) * (value.size.height / 100) * (conversionFactor)) //Current units are in cm, Devide by 100 to get units in meters                    
+                    /* for debuging
+                    console.log('\n*** ' + value.title + ' ***')
+                    console.log('Width: ' + value.size.width / 100)
+                    console.log('Length: ' + value.size.length / 100)
+                    console.log('Height: ' + value.size.height / 100)
+                    */
+                }
+                catch(error)
+                {
+                    //this can be handled better, but this is a small exercise so just making sure here if response is valid
+                    hasError = true
+                }
             }
         }
         );
 
-        let urlAddress = resp.data.next
-        if(urlAddress != null)
+        let nextUrlAddress = resp.data.next //Get next page url part
+        if(!hasError && nextUrlAddress != null)
         {
-            getProducts(urlAddress)
+            //Recursively fetch all products from api
+            computePrdAvgCuWgt(nextUrlAddress, filterItem)
         }
         else
         {
-            console.log('\nComputed Average Cubic Weight for ' +filterItem+ ': ' + Math.round((averageCubicWeight / itemCount + Number.EPSILON) * 100) / 100 + ' Kg\n')
-
+            //Result
+            hasError ? console.log('Cannot compute due to error\n') : console.log('\nComputed Average Cubic Weight for ' +filterItem+ ': ' + Math.round((itemCubicWeight / itemCount + Number.EPSILON) * 100) / 100 + ' Kg\n') //Rounding to 2 decmials            
         }
     });
 }
 
-getProducts(defaultUrlAddress)
+//Initiate fetch product from api
+computePrdAvgCuWgt(defaultUrlPart, filterItem)
 
